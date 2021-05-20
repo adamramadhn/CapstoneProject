1fe3740cd4afb0de1ab5827af6b89f18bd2fe287
whitefan <whitefan911@gmail.com>
2021-05-20 13:27:43 +0700

    DetailFragment updated

--- app/src/main/java/com/akiramenaide/capstoneproject/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/akiramenaide/capstoneproject/ui/detail/DetailFragment.kt
@@ -5,17 +5,73 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.akiramenaide.capstoneproject.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.akiramenaide.capstoneproject.core.data.entity.FruitEntity
+import com.akiramenaide.capstoneproject.core.util.DummyFruit
+import com.akiramenaide.capstoneproject.databinding.FragmentDetailBinding
+import com.akiramenaide.capstoneproject.ui.util.MyColors
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import java.util.ArrayList
 
 
 class DetailFragment : Fragment() {
+    private val brightColors = MyColors.brightColorArray
+    private lateinit var binding: FragmentDetailBinding
+    private lateinit var pieChart: PieChart
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (activity != null) {
+            val fruits = DummyFruit.getAllFruits()
+            pieChart = binding.pieChart
+            drawPieChart(fruits)
+            drawRecyclerView(fruits)
+        }
+    }
+
+    private fun drawPieChart(fruits: List<FruitEntity>){
+        val pieValues = ArrayList<PieEntry>()
+        for (element in fruits) {
+            pieValues.add(PieEntry(element.total.toFloat(), element.name))
+        }
+        val dataSet = PieDataSet(pieValues, "")
+
+        pieChart.apply {
+            data = PieData(dataSet).apply {
+                setValueFormatter(PercentFormatter())
+                setValueTextSize(12f)
+            }
+            isDrawHoleEnabled = false
+            setDrawEntryLabels(false)
+            isRotationEnabled = false
+            animateY(1000)
+        }
+        dataSet.colors = brightColors
+    }
+
+    private fun drawRecyclerView(fruits: List<FruitEntity>){
+        val fruitAdapter = DetailAdapter()
+        fruitAdapter.setFruitsData(fruits)
+        fruitAdapter.notifyDataSetChanged()
+
+        with(binding.rvFruits) {
+            layoutManager = LinearLayoutManager(context)
+            adapter = fruitAdapter
+            setHasFixedSize(true)
+        }
     }
 
 }
---
+++ app/src/main/java/com/akiramenaide/capstoneproject/ui/util/MyColors.kt
@@ -0,0 +1,16 @@
+package com.akiramenaide.capstoneproject.ui.util
+
+import android.graphics.Color
+
+object MyColors {
+    val brightColorArray = arrayListOf(
+        Color.YELLOW,
+        Color.GREEN,
+        Color.MAGENTA,
+        Color.CYAN
+    )
+    val stackedColorArray = arrayListOf(
+        Color.WHITE,
+        Color.BLACK
+    )
+}
